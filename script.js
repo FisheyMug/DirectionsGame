07a5eb2eaa0e187210d70e8e410626a7cbbbf705
Matthew <mattnfowler@gmail.com>
2022-10-14 14:01:49 +0900

    added index, style and script, set up basic skeloton of project, stated coding the script

---
+++ script.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+const straightBtn = document.querySelector('#straight');
+const rightBtn = document.querySelector('#right');
+const leftBtn = document.querySelector('#left');
+let img= new Image();
+img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
