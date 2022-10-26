4137d3725cc0805c3bcd9da8850de9ad51965ead
Matthew <mattnfowler@gmail.com>
2022-10-26 14:10:16 +0900

    added local file for sprite image

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let img= new Image();
-img.src = "images/Green-Cap-Character-16x18.png";
+img.src = "Green-Cap-Character-16x18.png";
 let player=1;
 // variables for moving
 let spriteY = 430;
